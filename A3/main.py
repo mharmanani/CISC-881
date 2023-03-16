b15ce96d2520ad816c72a7a4b25b229d4234fe0f
mharmanani <mharmanani@queensu.ca>
2023-03-16 09:35:47 -0400

    modify experiments code

--- A3/main.py
+++ A3/main.py
@@ -1,5 +1,4 @@
 from preprocessing import *
-from dataloaders import SegmentationDataset
 
 from model import UNet2D
 
@@ -11,18 +10,35 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 import time
 
+import torch.nn.functional as F
+
+import torch.nn as nn
+
 torch.manual_seed(88)
 
-# Source:
+# Sources:
     # - https://discuss.pytorch.org/t/calculating-dice-coefficient/44154
-def dice_score_coeff(pred, target):
-    eps = 1e-9
-    num = pred.size(0)
-    m1 = pred.view(num, -1).float()  # Flatten
-    m2 = target.view(num, -1).float()  # Flatten
-    intersection = (m1 * m2).sum().float()
+    # - https://discuss.pytorch.org/t/implementation-of-dice-loss/53552
+def dice_score_coeff(pred, target, smooth=1.):
+    """This definition generalize to real valued pred and target vector.
+    This should be differentiable.
+    pred: tensor with first dimension as batch
+    target: tensor with first dimension as batch
+    """
 
-    return (2. * intersection) / (m1.sum() + m2.sum() + eps)
+    # have to use contiguous since they may from a torch.view op
+    iflat = pred.contiguous().view(-1)
+    tflat = target.contiguous().view(-1)
+    intersection = (iflat * tflat).sum()
+
+    eps = 1e-9
+
+    A_sum = torch.sum(tflat * iflat)
+    B_sum = torch.sum(tflat * tflat)
+
+    dice = ((2. * intersection + smooth) / (A_sum + B_sum + eps + smooth) )
+    
+    return dice
 
 def prepare_input(slices, masks, pad=True):
     slices = slices.to(device).unsqueeze(1)
@@ -44,7 +60,7 @@ def get_batches(data_loader, batch_size):
 
 def train(model, train_data, valid_data, learning_rate, batch_size, num_epochs, exp_name):
     
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-3)
 
     train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size)
     valid_loader = torch.utils.data.DataLoader(valid_data, batch_size=batch_size)
@@ -83,10 +99,6 @@ def train(model, train_data, valid_data, learning_rate, batch_size, num_epochs, 
         torch.save(model.state_dict(), "weights/{0}_epoch_{1}.pth".format(exp_name, epoch+1))
         epochs_array.append(epoch+1)
 
-    print(epochs_array)
-    print(avg_train_losses)
-    print(avg_valid_losses)
-    
     plt.figure(figsize=(10, 5))
     plt.plot(epochs_array, avg_train_losses, label="Training Loss")
     plt.plot(epochs_array, avg_valid_losses, label="Validation Loss")
@@ -96,10 +108,14 @@ def train(model, train_data, valid_data, learning_rate, batch_size, num_epochs, 
     plt.savefig("{0}_loss.png".format(exp_name))
 
 
-def evaluate(model, test_data, batch_size=1):
+def evaluate(model, test_data, batch_size=1, from_epoch=None, experiment_name=''):
     model.eval()
     dices_coeffs = []
     test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size)
+    
+    if from_epoch:
+        model.load_state_dict(torch.load("weights/{0}_epoch_{1}.pth".format(experiment_name, from_epoch)))
+
     with torch.no_grad():
         for i, (slices, masks) in enumerate(test_loader):
             slices, masks = prepare_input(slices, masks)
@@ -107,43 +123,90 @@ def evaluate(model, test_data, batch_size=1):
             dsc = dice_score_coeff(outputs, masks)
             dices_coeffs.append(dsc.detach().cpu().item())
     avg_dsc = sum(dices_coeffs) / len(dices_coeffs)
-    print(f"[Test Dice Score Coefficient: {avg_dsc: .2f}]")
 
     return avg_dsc
 
-def experiment(desig, label_type, from_epoch=None, experiment_name=''):
+def experiment(model, desig, label_type, batch_size=16, from_epoch=None, 
+               experiment_name='', eval_only=False, num_epochs=10, learning_rate=1e-3):
     assert label_type[:-1] in ["whole_gland", "lesions"]
     assert desig in ["adc", "hbv", "t2w"]
 
     train_data, valid_data, test_data = stratify(desig, label_type, [(1,), (3,), (0,)])
-
-    model = UNet2D(device=device)
     
     if from_epoch:
         model.load_state_dict(torch.load("weights/{0}_epoch_{1}.pth".format(experiment_name, from_epoch)))
 
-    train(model, train_data, valid_data, 0.01, 4, 15, experiment_name)
-    
-    avg_test_dsc = evaluate(model, test_data, batch_size=4)
-    avg_train_dsc = evaluate(model, train_data, batch_size=4)
-    avg_valid_dsc = evaluate(model, valid_data, batch_size=4)
+    if not eval_only:
+        train(model, train_data, valid_data, learning_rate, batch_size, num_epochs, experiment_name)
 
-    print(f"[Test Dice Score Coefficient: {avg_test_dsc: .2f}]")
-    print(f"[Train Dice Score Coefficient: {avg_train_dsc: .2f}]")
-    print(f"[Valid Dice Score Coefficient: {avg_valid_dsc: .2f}]")
+    avg_test_dsc = evaluate(model, test_data, batch_size=batch_size)
+    avg_train_dsc = evaluate(model, train_data, batch_size=batch_size)
+    avg_valid_dsc = evaluate(model, valid_data, batch_size=batch_size)
+
+    print("{0} Results".format(experiment_name))
+    print(f"[Test Dice Score Coefficient: {avg_test_dsc: .6f}]")
+    print(f"[Train Dice Score Coefficient: {avg_train_dsc: .6f}]")
+    print(f"[Valid Dice Score Coefficient: {avg_valid_dsc: .6f}]")
 
 def main():
+    ############################
+    ## Question 9 Experiments ##
+    ############################
 
-    # Question 9 Experiments
-    experiment("t2w", "whole_gland/", experiment_name="t2w_whole_gland_9a", from_epoch=4)
-    #experiment("adc", "lesions/", experiment_name="adc_lesions_9b")
-    #experiment("hbv", "lesions/", experiment_name="hbv_lesions_9c")
+    # T2W Whole Gland Segmentation
+    model = UNet2D(device=device)
+    experiment(model, "t2w", "whole_gland/", batch_size=40, experiment_name='t2w_whole_gland_9a')
 
+    # ADC Cancer Lesions Segmentation
+    model = UNet2D(device=device)
+    experiment(model, "adc", "lesions/", batch_size=40, experiment_name='adc_lesions_9b')
+
+    # HBV Cancer Lesions Segmentation
+    model = UNet2D(device=device)
+    experiment(model, "hbv", "lesions/", batch_size=40, experiment_name='hbv_lesions_9c')
+
+    ###################################
+    ## Question 10 Ablation Studies  ##
+    ###################################
+
+    # Ablation experiment on the learning rate
+    model = UNet2D(device=device)
+    experiment(model, "adc", "lesions/", batch_size=40, learning_rate=1e-4, experiment_name='adc_lesions_10_lr1')
     
-
-    # Question 10 Ablation Experiments
-    pass # TODO: Implement ablative experiments
+    model = UNet2D(device=device)
+    experiment(model, "adc", "lesions/", batch_size=40, learning_rate=1e-2, experiment_name='adc_lesions_10_lr2')
     
+    model = UNet2D(device=device)
+    experiment(model, "hbv", "lesions/", batch_size=40, learning_rate=1e-4, experiment_name='hbv_lesions_10_lr1')
+    
+    model = UNet2D(device=device)
+    experiment(model, "hbv", "lesions/", batch_size=40, learning_rate=1e-2, experiment_name='hbv_lesions_10_lr2')
+    
+    model = UNet2D(device=device)
+    experiment(model, "t2w", "whole_gland/", batch_size=40, learning_rate=1e-4, experiment_name='t2w_whole_gland_10_lr1')
+    
+    model = UNet2D(device=device)
+    experiment(model, "t2w", "whole_gland/", batch_size=40, learning_rate=1e-2, experiment_name='t2w_whole_gland_10_lr2')
+
+    model = UNet2D(device=device)
+    experiment(model, "adc", "lesions/", batch_size=32, learning_rate=1e-3, experiment_name='adc_lesions_10_bs32')
+    
+    model = UNet2D(device=device)
+    experiment(model, "adc", "lesions/", batch_size=16, learning_rate=1e-3, experiment_name='adc_lesions_10_bs16')
+    
+    model = UNet2D(device=device)
+    experiment(model, "hbv", "lesions/", batch_size=32, learning_rate=1e-3, experiment_name='hbv_lesions_10_bs32')
+    
+    model = UNet2D(device=device)
+    experiment(model, "hbv", "lesions/", batch_size=16, learning_rate=1e-3, experiment_name='hbv_lesions_10_bs16')
+    
+    model = UNet2D(device=device)
+    experiment(model, "t2w", "whole_gland/", batch_size=32, learning_rate=1e-3, experiment_name='t2w_whole_gland_10_bs32')
+    
+    model = UNet2D(device=device)
+    experiment(model, "t2w", "whole_gland/", batch_size=16, learning_rate=1e-3, experiment_name='t2w_whole_gland_10_bs16')
+
+
 
 
 if __name__ == "__main__":
